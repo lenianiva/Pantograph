7e28ded23fd581e863dc718d71f7b0b78ee97a7f
Leni Aniva <v@leni.sh>
2024-03-06 15:14:08 -0800

    test: More diagnostics for tests

--- Pantograph/Protocol.lean
+++ Pantograph/Protocol.lean
@@ -242,7 +242,7 @@ structure GoalPrintResult where
   -- The root expression
   root?: Option Expression := .none
   -- The filling expression of the parent goal
-  parent?: Option Expression := .none
+  parent?: Option Expression
   deriving Lean.ToJson
 
 -- Diagnostic Options, not available in REPL
